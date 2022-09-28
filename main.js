0eac3615b3119d28e643c4d15c13aef7744dc07b
ShauryashenoyR <95034778+ShauryashenoyR@users.noreply.github.com>
2022-09-28 20:59:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload(){
+
+}
+
+
+function setup(){
+    canvas = createcanvas(300, 300);
+    canvas.center();
+}
+
+
+function draw(){
+
+}
+
+
+function take_snapshot(){
+    save('myFilterImage.png')
+}
